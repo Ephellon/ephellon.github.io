a461c0cc3c08369d776c843842b7a7ac30ebf0e9
Ephellon <minkcbos@gmail.com>
2017-09-21 22:49:53 -0500

    Update mio.js

--- Mio/js/mio.js
+++ Mio/js/mio.js
@@ -1,18 +1,15 @@
 var Mio = window.Mio = Mio || {power: 8},
     atob, btoa;
 
-// Base64 RegExp
-(atob || {}).regexp = /^[a-z\d\+\/]+[\=]{0,2}$/i;
-(atob || {}).characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/=";
-
 // Encode (Compress)
 Mio.Mi =
 Mio.enc =
+Mio.com =
 // String: String[, Boolean]
-function Mi(input, unbloat) {
+function Mi(input, bloated) {
   input = btoa(LZW_Wrap(LZW_Encode(input)));
 
-  return (!unbloat)? Mio.unbloat(input): input;
+  return (!bloated)? Mio.unbloat(input): input;
 };
 
 // Decode (Decompress)
@@ -30,15 +27,13 @@ function unbloat(string) {
   var characters = atob.characters,
       list       = {},
       media      = [],
-      output     = [];
+      output     = [],
+      z6         = "000000",
+      z8         = "00000000";
 
   if(atob.regexp.test(string))
     for(var index = 0, length = characters.length, character, code, next; index < length; index++)
-      list[( character = characters[index] )] = list[character] || (
-        (character == characters[64])?
-          "00":
-        ("000000" + index.toString(2)).slice(-6)
-      );
+      list[( character = characters[index] )] = list[character] || (z6 + index.toString(2)).slice(-6);
   else
     return string;
 
@@ -47,11 +42,16 @@ function unbloat(string) {
   media = media.join("").split(/([01]{8})/).join(",").replace(/^,|,$/g, "").split(/,,?/);
 
   for(index = 0, length = media.length; index < length; index++)
-    if(( code = media[index], next = media[index + 1] ) != "00" || next == undefined)
+    if((code = media[index]).length == 8)
       output.push(String.fromCharCode(+( "0b" + code )));
 
   return output.join("");
-}
+};
+atob = atob || Mio.unbloat;
+
+// Base64 RegExp
+atob.regexp = Mio.unbloat.regexp = /^[a-z\d\+\/]+[\=]{0,2}$/i;
+atob.characters = Mio.unbloat.characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
 // Unbloated string -> Base64 string
 Mio.bloat =
@@ -60,24 +60,64 @@ function bloat(string) {
   var characters = atob.characters,
       list       = {},
       media      = [],
-      output     = [];
+      output     = [],
+      z6         = "000000",
+      z8         = "00000000";
 
   for(var index = 0, length = characters.length, character, code, next; index < length; index++)
-    list[code = (index < 64? ("000000" + index.toString(2)).slice(-6): "00")] = list[code] || characters[index];
-  list["0000"] = list["00"] + list["00"];
+    list[code = (z6 + index.toString(2)).slice(-6)] = list[code] || characters[index];
 
   for(index = 0, length = string.length; index < length; index++)
-    media.push(( "00000000" + string.charCodeAt(index).toString(2) ).slice(-8));
+    media.push(( z8 + string.charCodeAt(index).toString(2) ).slice(-8));
   media = media.join("").split(/([01]{6})/).join(",").replace(/^,|,$/g, "").split(/,,?/);
 
   for(index = 0, length = media.length; index < length; index++)
-    if(( code = media[index] ).length == 6)
-      output.push(characters[+("0b" + code)]);
-    else
-      output.push(list[code]);
+    output.push(characters[+("0b" + (media[index] + z6).slice(0, 6))]);
 
   return output.join("");
-}
+};
+btoa = btoa || Mio.bloat;
+
+// Generate a hash signature
+Mio.sign =
+// String: String[, Number]
+function sign(string, fidelity) {
+  var array = (string + "").split(/([^]{16})/),
+      gamma = 0;
+
+  fidelity = 18 - Math.floor((fidelity || 0) * 16);
+
+  array.forEach(function(value, index, self) {
+    return (value == "")?
+      self.splice(index, 1):
+    gamma += value.charCodeAt(0);
+  });
+
+  for(var index = 0, length = array.length, last = length - 1, result = [], code = "charCodeAt"; index < length; index++)
+    for(var self = array[index], next = (array[index + 1] || ""), mirror = array[last], a, b, c, d, e, f, g = gamma, i = 0, j = self.length, k = mirror.length, l = length, m = k - 1, q = fidelity; i < j; ++i, --m, g = gamma += a + b + c + d + e + f)
+      a = self[code](i)         | 0,
+      b = self[code](j - i - 1) | 0,
+      c = mirror[code](m)       | 0,
+      d = mirror[code](k - m)   | 0,
+      e = next[code](i)         | 0,
+      f = next[code](m)         | 0,
+      result.push(Math.abs(
+        (((a ^ ~b) << (i + k)) |  (j & e) | g) ^
+        (((b | -c) ^  (m + j)) |  (j & f) | g) ^
+        (((c & ~d) << (e - k)) >> (k ^ q) + g) ^
+        (((d << a) ^  (f - j)) >> (k ^ q) + g) ^
+
+        ((a & b | c ^ d) ^ e - f) << (q & e & f)
+      ));
+
+  result.splice(fidelity, result.length - fidelity);
+
+  result.forEach(function(value, index, self) {
+    return self.splice(index, 1, (value ^ gamma).toString(36));
+  });
+
+  return result.join("");
+};
 
 // LZW from [JSFiddle](https://jsfiddle.net/ryanoc/fpMM6/)
 // LZW-compress a string
@@ -157,10 +197,7 @@ function LZW_Wrap(input) {
   // console.warn("[Mi/o]: Set Mio.power to " + pow + " for '" + uni + "', '\\u" + f(uni.charCodeAt(0).toString(16)) + "'");
 
   for(var i = 0, j; i < input.length; i++)
-    media.push(
-      "0".repeat(pow - (j = input.charCodeAt(i).toString(2)).length) +
-      j
-    );
+    media.push("0".repeat(pow - (j = input.charCodeAt(i).toString(2)).length) + j);
 
   media = media.join("").split(/([01]{8})/);
 
