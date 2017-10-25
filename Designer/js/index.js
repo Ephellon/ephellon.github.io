afd2989cdfaaed6b9b5d0aa85aceee36ff82715b
Ephellon <minkcbos@gmail.com>
2017-10-24 19:22:44 -0500

    Update index.js

--- Designer/js/index.js
+++ Designer/js/index.js
@@ -41,11 +41,11 @@ $(document).ready(function() {
       ]
     }, {
       title: 'Rihanna: ANTI',
-      text: '<p>An edited version of Rihanna\'s <a href="http://www.rihannanow.com/music/anti/" target="blank_">ANTI</a> album art, which I thought should be her deluxe version\'s cover art.</p>',
+      text: '<p>An edited version of Rihanna\'s <a href="http://www.rihannanow.com/music/bitch-better-have-my-money/" target="blank_">Bitch Better Have My Money</a> album art./p>',
 
-      image: ['https://ephellon.github.io/Designer/img/ANTI.png'],
+      image: ['https://ephellon.github.io/Designer/img/Bitch Better Have My Money.jpg'],
       site: [
-        ['Link to Image', 'fa-link', 'https://ephellon.github.io/Designer/img/ANTI.png']
+        ['Link to Image', 'fa-link', 'https://ephellon.github.io/Designer/img/Bitch Better Have My Money.jpg']
       ]
     }, {
       title: 'Rihanna: ANTI (Alternate)',
@@ -54,6 +54,14 @@ $(document).ready(function() {
       image: ['https://ephellon.github.io/Designer/img/ANTI (Alternate).png'],
       site: [
         ['Link to Image', 'fa-link', 'https://ephellon.github.io/Designer/img/ANTI (Alternate).png']
+      ]
+    }, {
+      title: 'Rihanna: Bitch Better Have My Money',
+      text: '<p>Preffered album art for Cardi B\'s <a target="blacnk_" href="https://itunes.apple.com/us/album/bodak-yellow-feat-kodak-black-single/id1286929334">Bodak Yellow (iTunes)</a>.</p>',
+
+      image: ['https://ephellon.github.io/Designer/img/Bodak Yellow.png'],
+      site: [
+        ['Link to Image', 'fa-link', 'https://ephellon.github.io/Designer/img/Bodak Yellow.png']
       ]
     }, {
       title: 'J. Cole: Born Sinner',
