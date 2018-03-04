199272e2f01ba34156487350be22218c1e1bc31e
Ephellon <minkcbos@gmail.com>
2018-03-03 22:20:08 -0600

    Update synq.js

--- GoPass/html/js/synq.js
+++ GoPass/html/js/synq.js
@@ -231,7 +231,7 @@ SynQ.clear = function() {
 
 // Lock and unlock data (weak security)
 SynQ.lock = function(data, key) {
-  key = SynQ.salt(key);
+  key = SynQ.sign(SynQ.salt(key), 0);
 
   for(var index = 0, salted = []; index < data.length; index++)
     salted.push(String.fromCharCode(
