df465fd8b85ec7b08f1dbf0d252cd709ede33480
Ephellon <minkcbos@gmail.com>
2018-03-03 22:12:06 -0600

    Update synq.js

--- GoPass/html/js/synq.js
+++ GoPass/html/js/synq.js
@@ -1,91 +1,107 @@
-/** Ephellon Dantzler - 2015
-  * You call on SynQ() and that's it
-  * If you don't want to update element's, just some data, then use SynQ.push, SynQ.pull or SynQ.pop
-  * If youd like to get rid of all of your data (for one site) then use SynQ.clear
-  * If you'd like to synchronize across your entire domain, then set the use_global_synq_token to a defined value
+/** Ephellon Dantzler - 2015, 2018
+  * How to use ([...] is optional):
+  * Simply call on SynQ([attribute-name]), and that's it
+  * FAQ:
+  * Q1) What if I don't want to update elements, just some data?
+  * A2) Use SynQ.push(string:name, string:data[, string:password]),
+        SynQ.pull(string:name[, string:password]),
+        OR SynQ.pop(string:name[, string:password])
+  * Q2) What if I'd like to get rid of all of my data (in SynQ)?
+  * A2) Use SynQ.clear()**
+  * Q3) What if I'd like to synchronize across my entire domain?
+  * A3) Set the use_global_synq_token to a defined value
+  * Q4) How do I check on the status of my data?
+  * A4) You can use SynQ.data([boolean:show-private]) to show your data
+  * Q5) What about the other things I see under SynQ?
+  * A6) Those are for future technologies, but you can use them as you see fit.
+  *
+  * ** SynQ.clear will only remove local*** items if the "use_global_synq_token" isn't set
+  * *** By "local" I mean for a unique page identifier (URL), e.g. "https://example.com/page-1" won't share "local" data with ".../page-2"
   */
 
-var SynQName = "synq-data synq-text synq-html".split(" "),
-      // change this accordingly; this is the syncronizing attribute for your elements
-      // synq-data: .innerText OR .value
-      // synq-text: .innerHTML
-      // synq-html: .outerHTML
-    SynQESC  = "<!-- synq-delimeter -->",
-      // change to your liking; this is the list delimeter
-    doc      = document,
-    win      = window,
-    los      = localStorage,
-    noname   = "Resource name is missing.",
-    sig, use_global_synq_token;
-
-doc.Qy = doc.querySelectorAll;
+var NO_NAME = "Resource name is missing.",
+    use_global_synq_token;
 
 // IE and Edge?
-if(!("addEventListener" in win))
-  win.addEventListener = win.attachEvent;
+if(!("addEventListener" in window))
+  window.addEventListener = window.attachEvent;
 
 // The main function
 function SynQ(name) {
-  name = name || SynQName.join("],[");
+  name = name || SynQ.syn.join("],[");
 
   var messages = [],
       uuids    = [],
       copies   = {},
-      query    = doc.Qy("[" + name + "]");
+      query    = document.querySelectorAll("[" + name + "]");
 
   function fetch(e) {
-    var n, a = e.attributes;
-
-    if("synq-data" in a)
-      if("value" in e)
-        n = "value";
-      else
-        n = "innerText";
-    else if("synq-text" in a)
-      n = "innerHTML";
-    else
-      n = "outerHTML";
+    var a = e.attributes,
+        n =
+    ("synq-data" in a)?
+      ("value" in e)?
+        "value":
+      "innerText":
+    ("synq-text" in a)?
+      "innerHTML":
+    "outerHTML";
 
     return e[n];
   }
 
-  for(var index = 0, element, uuid, value, info; index < query.length; index++)
+  for(var index = 0, element, uuid, value, info, id; index < query.length; index++) {
     // Element information
-    info = (element = query[index]).tagName,
-    // Add copies of elements for the UUID to work properly
-    info += ((element.attributes.id)?
-      "#" + element.attributes.id:
-    ":nth-child(" + (copies[info] |= 0, ++copies[info]) + ")"),
-    // Push the element's UUID
-    uuids.push(uuid = SynQ.sign(info, 1)),
-    // Set the element's UUID for future references
-    element.setAttribute("uuid", uuid),
-    // Push the messages
-    messages.push(value = fetch(element)),
-    SynQ.push(".values#" + uuid, value);
+    copies[info] |= 0;
+    element       = query[index];
+    info          = element.tagName;
+    id            = element.attributes.id;
 
-  SynQ.push(".values", messages.join(SynQESC));
+    // Add copies of elements for the UUID to work properly
+    info += (id != undefined && id != null)?
+      "#" + id:
+    ":nth-child(" + (++copies[info]) + ")";
+
+    // Push the element's UUID
+    uuid = SynQ.sign(info, 1);
+    uuids.push(uuid);
+
+    // Set the element's UUID for future references
+    element.setAttribute("uuid", uuid);
+
+    // Push the messages
+    value = fetch(element);
+    messages.push(value);
+    SynQ.push(".values#" + uuid, value);
+  }
+
+  SynQ.push(".values", messages.join(SynQ.esc));
   SynQ.push(".uuids", uuids.join(","));
 
   SynQ.eventlistener("SynQ.");
 };
 
+SynQ.syn = "synq-data synq-text synq-html".split(" ");
+  // change this accordingly; this is the syncronizing attribute for your elements
+  // synq-data: .innerText OR .value
+  // synq-text: .innerHTML
+  // synq-html: .outerHTML
+SynQ.esc  = "<!-- synq-delimeter -->";
+  // change to your liking; this is the list delimeter
+
 // The event-listener
 SynQ.eventlistener = function(event) {
-  var query = doc.Qy("[" + SynQName.join("],[") + "]");
+  var query = document.querySelectorAll("[" + SynQ.syn.join("],[") + "]");
 
   function write(e, m) {
-    var n, a = e.attributes;
-
-    if("synq-data" in a)
-      if("value" in e)
-        n = "value";
-      else
-        n = "innerText";
-    else if("synq-text" in a)
-      n = "innerHTML";
-    else
-      n = "outerHTML";
+    var a = e.attributes,
+        n =
+    ("synq-data" in a)?
+      ("value" in e)?
+        "value":
+      "innerText":
+    ("synq-text" in a)?
+      "innerHTML":
+    "outerHTML";
 
     e[n] = m;
   }
@@ -97,11 +113,11 @@ SynQ.eventlistener = function(event) {
 
     if(values == undefined || values == null)
       break update;
-    values = values.split(SynQESC);
+    values = values.split(SynQ.esc);
 
     // UUID is set
     // Write confidently, even if the HTML document has changed
-    if(element.uuid)
+    if(element.uuid != undefined && element.uuid != null)
       value = SynQ.pull(".values#" + element.uuid) || values[index],
       write(element, value);
     // UUID isn't set
@@ -154,98 +170,100 @@ SynQ.data = function(all) {
                + ")" + (all? ".+": "[^\\.].+") + "$"),
       array  = {};
 
-  for(item in los)
+  for(item in localStorage)
     if(regexp.test(item))
-      array[item] = los[item];
+      array[item] = localStorage[item];
 
   return array;
 };
 
 // Push (set) a resource
 SynQ.push = function(name, data, key) {
-  SynQ.prevent(name, [undefined, null], noname);
+  SynQ.prevent(name, [undefined, null], NO_NAME);
 
-  if(key != undefined)
+  if(key != undefined && key != null)
     data = SynQ.lock(data, key),
     key  = ".";
   else
     key = "";
 
-  los.setItem(sig + key + name, data);
+  localStorage.setItem(SynQ.signature + key + name, data);
 
   return data;
 };
 
 // Pull (get) a resource
 SynQ.pull = function(name, key) {
-  SynQ.prevent(name, [undefined, null], noname);
+  SynQ.prevent(name, [undefined, null], NO_NAME);
 
   var data;
 
-  if(key != undefined)
-    data = SynQ.unlock(los.getItem(sig + "." + name), key);
+  if(key != undefined && key != null)
+    data = SynQ.unlock(localStorage.getItem(SynQ.signature + "." + name), key);
   else
-    data = los.getItem(sig + name);
+    data = localStorage.getItem(SynQ.signature + name);
 
   return data;
 };
 
 // Remove a resource
 SynQ.pop = function(name, key) {
-  SynQ.prevent(name, [undefined, null, ""], noname);
+  SynQ.prevent(name, [undefined, null, ""], NO_NAME);
 
   var data = SynQ.pull(name, key);
 
-  if(key != undefined)
-    los.removeItem(sig + "." + name);
+  if(key != undefined && key != null)
+    localStorage.removeItem(SynQ.signature + "." + name);
   else
-    los.removeItem(sig + name);
+    localStorage.removeItem(SynQ.signature + name);
 
   return data;
 };
 
 // The "clear all" option
 SynQ.clear = function() {
-  var regexp = RegExp("^" + sig.replace(/(\W)/g, "\\$1") + "$");
+  var regexp = RegExp("^" + SynQ.signature.replace(/(\W)/g, "\\$1") + "$");
 
-  for(item in los)
+  for(item in localStorage)
     if(regexp.test(item))
-      los.removeItem(item);
+      localStorage.removeItem(item);
 };
 
 // Lock and unlock data (weak security)
 SynQ.lock = function(data, key) {
   key = SynQ.salt(key);
 
-  for(var i = 0, S = []; i < data.length; i++)
-    S.push(String.fromCharCode(
-      data.charCodeAt(i) ^ key.charCodeAt(i % 256)
+  for(var index = 0, salted = []; index < data.length; index++)
+    salted.push(String.fromCharCode(
+      data.charCodeAt(index) ^ key.charCodeAt(index % 256)
     ));
 
-  return S.join("");
+  return salted.join("");
 };
 
 SynQ.unlock = function(data, key) {
   return SynQ.lock(data, key);
 };
 
+// KSA algorithm
 SynQ.salt = function(text) {
-  for(var i = 0, j = 0, k = 0, S = []; i < 256; i++)
-    S[i] = i;
+  for(var index = 0, exdex = 0, swap = 0, salted = []; index < 256; index++)
+    salted[index] = index;
 
-  for(i = 0; i < 256; i++)
-    j = (j + S[i] + text.charCodeAt(i % text.length)) % 256,
-    S[k] = S[i],
-    S[i] = S[j],
-    S[j] = S[k];
+  for(index = 0; index < 256; index++)
+    exdex = (exdex + salted[index] + text.charCodeAt(index % text.length)) % 256,
+    salted[swap]  = salted[index],
+    salted[index] = salted[exdex],
+    salted[exdex] = salted[swap];
 
-  for(i = 0; i < 256; i++)
-    S[i] = String.fromCharCode( S[i] );
+  for(index = 0; index < 256; index++)
+    salted[index] = String.fromCharCode( salted[index] );
 
-  return S.join("");
+  return salted.join("");
 };
 
 // The hash/signature function
+// From my Mi/o project
 SynQ.sign = function(string, fidelity) {
   var array = (string + "").split(/([^]{16})/),
       gamma = 0,
@@ -295,11 +313,11 @@ SynQ.prevent = function(variable, failures, message) {
       throw new Error(message);
 };
 
-// Auto-update
+// Auto-update & run
 if(use_global_synq_token == undefined)
-  sig = "synq://" + SynQ.sign(location, 1) + "/";
+  SynQ.signature = "synq://" + SynQ.sign(location, 1) + "/";
 else
-  sig = "synq://" + SynQ.sign(location.origin, 1) + "/";
+  SynQ.signature = "synq://" + SynQ.sign(location.origin, 1) + "/";
 
 SynQ.eventlistener("SynQ initializer.");
-win.addEventListener("storage", win.onstorage = SynQ.eventlistener, false);
+window.addEventListener("storage", window.onstorage = SynQ.eventlistener, false);
