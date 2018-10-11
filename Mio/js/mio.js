a700ea23270226e1a48adea39bdd3832234a71f5
Ephellon <minkcbos@gmail.com>
2018-10-11 17:30:26 -0500

    Update mio.js

--- Mio/js/mio.js
+++ Mio/js/mio.js
@@ -207,52 +207,78 @@ Mio.decompress = (string) => {
 
 // String -> Salted Hash Signature
 // String: String[, Number @Float{0...1}[, Boolean]]
-Mio.sign = (string, fidelity, method) => {
-  var printable = printable || Mio.text,
-      library = library || Mio.book,
-      array = (string = string + "").split(/([^]{16})/),
-      gamma = 0;
+Mio.sign =
+/** Siphun - hashing algorithm
+ * @author Ephellon Dantzler (Cliffton E.D. Tucker)
+ */
+function Siphun(string, fidelity) {
+  var array = (string += '').split(/([^]{256})/),
+      gamma = 0,
+      result = [],
+      method,
+      base;
 
-  fidelity = 18 - Math.floor((fidelity || 0) * 16);
-  method |= false;
+  fidelity |= 0;
 
-  if(!method)
-    method = function(s) {
-      return (s != undefined)? s.charCodeAt(0): s;
-    };
-  else
-    method = function(s) {
-      return (s != undefined)? printable.indexOf(s): s;
-    };
+  var R = function(t) {
+    var s = [], S = [], N = 256, l = (t = t || '').length;
 
-  array.forEach(function(value, index, self) {
-    return (value == "")?
-      self.splice(index, 1):
-    gamma += value.charCodeAt(0);
+    if(!l)
+      return '';
+    else
+      t = ' ' + t;
+
+    for(var n = 0; n < N;)
+      s.push(n++);
+
+    for(var n = 0, x = n, w = x; n < N; n++) {
+      x = (x + s[n] + t.charCodeAt(n % l)) % N;
+      s[w] = s[n];
+      s[n] = s[x];
+      s[x] = s[w];
+    }
+
+    for(var n = -1; n < N; n++)
+      S.push(t[s[n]] || '');
+
+    return S.join('');
+  };
+
+  fidelity = 2 + ((fidelity * 14) | 0);
+
+  method = function(s) { return s? s.charCodeAt(0): s };
+
+  array.forEach(function(value, index, array) {
+    return (value == '')?
+      (array = array || this).splice(index, 1):
+    gamma += (value.charCodeAt(0) % 13) | 0
   });
 
-  for(var index = 0, length = array.length, last = length - 1, result = []; index < length; index++)
-    for(var self = array[index], next = (array[index + 1] || ""), mirror = array[last], a, b, c, d, e, f, g = gamma, i = 0, j = self.length, k = mirror.length, l = length, m = k - 1, q = fidelity; i < j; ++i, --m, g = gamma += a + b + c + d + e + f)
-      a = method(self[i])         | 0,
-      b = method(self[j - i - 1]) | 0,
-      c = method(mirror[m])       | 0,
-      d = method(mirror[k - m])   | 0,
-      e = method(next[i])         | 0,
-      f = method(next[m])         | 0,
-      result.push(Math.abs(
+  for(var index = 0, length = array.length, last = length - 1; index < length; index++)
+    for(var self = array[index], next = (array[index + 1] || ""), mirror = array[last], a, b, c, d, e, f, g = gamma, i = 0, j = self.length, k = mirror.length, l = length, m = k - 1, q = fidelity; i < j; ++i, --m, gamma = g += a + b + c + d + e + f)
+      a = (method(self[i])         | q) | 0,
+      b = (method(self[j - i - 1]) - a) | 0,
+      c = (method(mirror[m])       + b) | 0,
+      d = (method(mirror[k - m])   ^ c) | 0,
+      e = (method(next[i])         | d) | 0,
+      f = (method(next[m])         ^ e) | 0,
+      result.push(
         (((a ^ ~b) << (i + k)) |  (j & e) | g) ^
         (((b | -c) ^  (m + j)) |  (j & f) | g) ^
         (((c & ~d) << (e - k)) >> (k ^ q) + g) ^
         (((d << a) ^  (f - j)) >> (k ^ q) + g) ^
 
         ((a & b | c ^ d) ^ e - f) << (q & e & f)
-      ));
+      );
 
   result.splice(fidelity, result.length - fidelity);
+  base = (((gamma | 32) % 20) + (fidelity % 16));
 
-  result.forEach((value, index, self) => {
-    return self.splice(index, 1, (value ^ gamma).toString((gamma % 20) + 16));
-  });
+  result.forEach(function(value, index, self) { return self.splice(index, 1, Math.abs(value ^ gamma).toString(base)) });
 
-  return result.join("").slice(0, 256);
+  result = result.join('').slice(0, 256);
+  gamma = gamma.toString(base);
+  base = (base * (fidelity || 1)).toString(base);
+
+  return R(result) + base + gamma;
 };
