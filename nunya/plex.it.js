d2b5b09d35ad61e571ef7e59534da835adcbb85e
Ephellon <minkcbos@gmail.com>
2018-11-12 14:51:47 -0600

    Update plex.it.js

--- nunya/plex.it.js
+++ nunya/plex.it.js
@@ -53,7 +53,7 @@
 			url += '&queue_email=';
 			url += '&plex=' + !!window.IAMPLEX;
       			// Serialized JSON data
-      			url += '&data=' + window.location.hash.replace('#plexit:', encodeURIComponent(window.location.href.replace(window.location.hash, '')) + '#');
+      			url += '&data=' + window.location.hash.replace('#plexit:', encodeURIComponent(window.location.href.replace(window.location.hash, '')));
 
 			var element = document.createElement('iframe');
 			element.id = frameId;
