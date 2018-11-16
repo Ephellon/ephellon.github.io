d35ed0f6c8916072c50e65a3eab28904b5c83eaf
Ephellon <minkcbos@gmail.com>
2018-11-16 12:17:44 -0600

    Update toloka.js

--- web.to.plex/plugins/toloka.js
+++ web.to.plex/plugins/toloka.js
@@ -18,22 +18,18 @@ const plugin = {
 
     // REQUIRED [plugin.init]: this is what Web to Plex will call on when the url is detected
     // it will always be fired after the page and Web to Plex have been loaded
-    init: (IMDbID) => {
+    init: () => {
         let title = document.querySelector('.maintitle').textContent.replace(/^[^\/]+\/\s+([^\(]+)\(([\d]{4})/, '$1').trim(),
                 // REQUIRED [title:string]
             year = RegExp.$2,
                 // PREFERRED [year:number, null, undefined]
-            image = document.querySelector('.postbody img').src;
+            image = document.querySelector('.postbody img').src,
                 // OPTIONAL [image:string]
+            id = plugin.getID();
+                // the rest of the code is up to you, but should be limited to a layout similar to this
 
         // REQUIRED [{ type:'movie', 'show'; title:string; year:number; target:'web-to-plex' }]
         // PREFERRED [{ image:string; IMDbID:string; TMDbID:string, number; TVDbID:string, number }]
         return { type: 'movie', title, year, image, IMDbID, url, target: 'web-to-plex' };
     }
 };
-
-// the rest of the code is up to you, but should be limited to a layout similar to this
-let id = plugin.getID();
-
-if(id)
-    return plugin.init(id);
