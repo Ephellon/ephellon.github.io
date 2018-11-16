f0ef1c0010aa8acada91bf099f5daff4c090a954
Ephellon <minkcbos@gmail.com>
2018-11-16 08:47:25 -0600

    Added commentating and rules
    Added comments to help guide future plugin developers

--- web.to.plex/plugins/toloka.js
+++ web.to.plex/plugins/toloka.js
@@ -2,24 +2,36 @@
  * Aurthor(s) - @chmez (2017)
  */
 
+// REQUIRED [const plugin:object]: The plugin constant, must be a constant
 const plugin = {
+    // REQUIRED [plugin.url]: this is what you ask Web to Plex access to; currently limited to a single domain
     url: "https://toloka.to/t*",
+
+    // OPTIONAL: this is purely for your code functionality
     getID: () => {
         let links = document.querySelectorAll('.postlink'),
-            regex = /^https?:\/\/(?:w{3}\.)?imdb\.com\/title\/(tt\d+)/;
+            regex = /^https?\:\/\/(?:w{3}\.)?imdb\.com\/title\/(tt\d+)/i;
 
         for(let link in links)
             if(regex.test(links[link]))
                 return RegExp.$1;
     },
-    init: (IMDbID) => {
-        let name = document.querySelector('.maintitle').textContent.replace(/^[^\/]+\/\s+([^\(]+)\(([\d]{4})/, '$1').trim(),
-            year = RegExp.$2;
 
+    // REQUIRED [plugin.init]: this is what Web to Plex will call on when the url is detected
+    // it will always be fired after the page and Web to Plex have been loaded
+    init: (IMDbID) => {
+        let title = document.querySelector('.maintitle').textContent.replace(/^[^\/]+\/\s+([^\(]+)\(([\d]{4})/, '$1').trim(),
+                // REQUIRED [title:string]
+            year = RegExp.$2;
+                // PREFERRED [year:number, null, undefined]
+
+        // REQUIRED [{ type:'movie', 'show'; title:string; year:number; target:'web-to-plex', 'web-to-plex:plexit' }]
+        // PREFERRED [{ IMDbID:string; TMDbID:string, number; TVDbID:string, number }]
         window.postMesage({ type: 'movie', title, year, IMDbID, target: 'web-to-plex' }, '*');
     }
 };
 
+// the rest of the code is up to you, but should be limited to a layout similar to this
 let id = plugin.getID();
 
 if(id)
