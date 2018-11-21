ae3a229f21ca111c35dea3a7d8f77eee16d66477
Ephellon <minkcbos@gmail.com>
2018-11-20 18:52:32 -0600

    Update shana_project.js

--- web.to.plex/plugins/shana_project.js
+++ web.to.plex/plugins/shana_project.js
@@ -6,7 +6,7 @@ let plugin = {
 
     "init": () => {
         let title = document.queryBy('.overview i, #header_big .header_info_block').first.textContent.trim(),
-            year = +document.queryBy('#header_big .header_info_block + *').first.textContent.trim().replace(/^.*(\d{4}).*$/m, '$1'),
+            year = +document.queryBy('#header_big .header_info_block + *').first.textContent.trim().replace(/[^]*(\d{4})[^]*/m, '$1'),
             image = document.queryBy('#header_big .header_display_box').first.style['background-image'].trim().replace(/url\((.+)\)/i, '$1');
 
         return { type: 'show', title, year, image };
